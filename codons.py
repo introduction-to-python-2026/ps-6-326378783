8c8319c76d9394d4772c669f9196bcfe76df6430
Bar johan <barjohan25.11@gmail.com>
2025-11-30 21:54:03 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,10 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
-
+    file = open(path, "r")
+    rows = file.readlines()
+    file.close()
+    empty_dict = {}
+    for row in rows:
+        row = row.strip().split("\t")
+        empty_dict[row[0]] = row [2]
+    return empty_dict
 
